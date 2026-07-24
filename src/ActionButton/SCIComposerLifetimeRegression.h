213862d67a5773625d7e55fbd906308c1529a001
Bigo <challengingfuture@gmail.com>
2026-07-24 05:23:45 +0300

    chore: remove redundant lifetime marker header

--- src/ActionButton/SCIComposerLifetimeRegression.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-// Marker header retained with SCIComposerLifetimeRegression.m so future source
-// syncs do not silently drop the ARC lifetime regression coverage.
-FOUNDATION_EXPORT NSString * const SCIComposerLifetimeRegressionMarker;
